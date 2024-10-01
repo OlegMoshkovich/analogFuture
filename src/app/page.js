e24ea3f975fba2735d5b4bd9f25824cb6ac06d0f
OlegMoshkovich <oleg.mosh@gmail.com>
2024-10-01 13:12:09 +0200

    add mobile support

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 
 export default function Page() {
   return (
-    <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
+    <div style={{ width: '100dvw',  height: '100dvh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
       <h1 style={{ fontSize: '1rem', fontWeight: 'bold', marginBottom: '2rem', marginTop: '2rem'}}>Analog Future</h1>
       <Home />
       <a href="https://www.pinterest.com/olegmoshkovich/analog-future/" target='_blank' style={{ position: 'absolute', bottom: '2rem', right: '2rem', fontSize: '0.8rem', color: 'grey' }}>Maybe it feels like this ..?</a>
