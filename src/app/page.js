9810628a740e3328c91e5c3c7ff4009cc68b15c8
OlegMoshkovich <oleg.mosh@gmail.com>
2024-11-15 12:27:02 +0000

    add the qrcode

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './Components/Home';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -7,11 +7,33 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
+
+// Custom hook to detect if the app is opened on a mobile device
+function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
+      setIsMobile(window.innerWidth <= 600 || isMobileDevice);
+    };
+
+    handleResize(); // Check on mount
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isMobile;
+}
 
 export default function Page() {
+  const isMobile = useIsMobile();
   const [isRotating, setIsRotating] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
 
   const handleTitleClick = () => {
     setIsRotating(!isRotating);
@@ -33,11 +55,58 @@ export default function Page() {
     setIsDialogOpen(false);
   };
 
+  const handleShareDialogOpen = () => {
+    setIsShareDialogOpen(true);
+  };
+
+  const handleShareDialogClose = () => {
+    setIsShareDialogOpen(false);
+  };
+
   return (
     <div style={{ width: '100dvw', height: '100dvh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
       <Home isRotating={isRotating} />
       {/* <a href="https://www.pinterest.com/olegmoshkovich/analog-future/" target='_blank' style={{ position: 'absolute', bottom: '2rem', right: '2rem', fontSize: '0.8rem', color: 'lightgrey' }}>Blog</a> */}
       <a onClick={handleDialogOpen} style={{ position: 'absolute', bottom: '2rem', left: '2rem', fontSize: '0.8rem', color: 'lightgrey', cursor: 'pointer' }}>About</a>
+      <Tooltip title={isMobile?'':"Share"}>
+        <Box
+          onClick={handleShareDialogOpen}
+          sx={{
+            position: 'absolute',
+            bottom: '2rem',
+            left: '5rem',
+            width: '12px',
+            height: '12px',
+            marginBottom: '3px',
+            backgroundColor: 'white',
+            border: '3px solid black',
+            cursor: 'pointer',
+            '@media (max-width: 600px)': {
+              left: '6rem',
+            },
+          }}
+        />
+      </Tooltip>
+      <Tooltip  title={isMobile?'':"Maybe it feels like this..?"}>
+        <Box
+          onClick={() => window.open('https://www.pinterest.com/olegmoshkovich/maybe-it-feels-like-this/', '_blank')}
+          sx={{
+            position: 'absolute',
+            bottom: '2rem',
+            right: '6.2rem',
+            width: '14px',
+            height: '12px',
+            marginBottom: '3px',
+            backgroundColor: 'black',
+            border: '3px solid black',
+            cursor: 'pointer',
+            '@media (max-width: 600px)': {
+              right: '2rem',
+              bottom: '4rem',
+            },
+          }}
+        />
+      </Tooltip>
       <Dialog
         open={isDialogOpen}
         onClose={handleDialogClose}
@@ -69,6 +138,40 @@ export default function Page() {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleDialogClose} color="primary" sx={{ color: 'white' }}>
+            <Typography variant="caption">Close</Typography>
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={isShareDialogOpen}
+        onClose={handleShareDialogClose}
+        PaperProps={{
+          sx: {
+            width: '610px',
+            backgroundColor: '#0a0a0a',
+            color: 'white',
+          },
+        }}
+      >
+        <DialogTitle variant="body2">Share</DialogTitle>
+        <DialogContent
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100px',
+            '@media (max-width: 600px)': {
+              height: '120px',
+            },
+          }}
+        >
+          {/* <Typography variant="body2">
+            Share this content with your friends!
+          </Typography> */}
+          <img src="/images/qrcode.png" alt="Share" style={{ width: '80px', height: '80px', marginTop: '10px' }} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleShareDialogClose} color="primary" sx={{ color: 'white' }}>
             <Typography variant="caption">Close</Typography>
           </Button>
         </DialogActions>
