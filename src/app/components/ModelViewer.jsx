3e037f45145c996ea35286df55a6898192dd5253
OlegMoshkovich <oleg.mosh@gmail.com>
2024-12-18 11:57:41 +0100

    left only three projects

--- src/app/components/ModelViewer.jsx
+++ src/app/components/ModelViewer.jsx
@@ -55,7 +55,7 @@ function RhinoModel({ url, isRotating, initialZoom, initialPosition, verticalOff
   useFrame(({ clock }) => {
     if (ref.current) {
       if (!initialRotationDone) {
-        const maxRotation = Math.PI / 40; // 10 degrees in radians
+        const maxRotation = Math.PI / 30; // 10 degrees in radians
         const speed = 0.02; // Adjust speed of rotation
         if (rotationProgress < 1) {
           ref.current.rotation.x = maxRotation * Math.sin(rotationProgress * Math.PI);
@@ -66,7 +66,7 @@ function RhinoModel({ url, isRotating, initialZoom, initialPosition, verticalOff
       } else if (isRotating) {
         const time = clock.getElapsedTime();
         const rotationAngle = Math.sin(time) * (Math.PI / 10);
-        ref.current.rotation.y = rotationAngle;
+        ref.current.rotation.x = rotationAngle;
       }
     }
   });
